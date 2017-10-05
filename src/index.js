ea8e8e77492248f2304d0ab3f0a0a8c4c65cd6db
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-05 15:01:24 -0600

    refactored weather table

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import WeatherComponent from "./Weather.js";
 import 'bootstrap/dist/css/bootstrap.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'react-fontawesome'; 
 
 ReactDOM.render(<WeatherComponent />, document.getElementById('root'));
 
